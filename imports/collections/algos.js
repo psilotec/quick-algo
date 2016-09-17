b4653ef29d490a4f08b1d42c1fff7d833c6b9f01
psilotec <scottadevito@gmail.com>
2016-09-16 20:41:00 -0400

    Create algos.insert Method

--- imports/collections/algos.js
+++ imports/collections/algos.js
@@ -3,4 +3,15 @@
  */
 import { Mongo } from 'meteor/mongo';
 
+Meteor.methods({
+    'algos.insert': function() {
+        return Algos.insert({
+            createdAt: new Date(),
+            content: '',
+            sharedWith: [],
+            ownerId: this.userId
+        });
+    }
+});
+
 export const Algos = new Mongo.Collection('algos');
