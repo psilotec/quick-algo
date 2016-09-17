130f815ee2ad9694a1e298676dc5a8ffd105e3e4
psilotec <scottadevito@gmail.com>
2016-09-16 20:52:03 -0400

    Set Up Publication for Algo List
    Remove autopublish package

--- server/main.js
+++ server/main.js
@@ -2,5 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import { Algos } from '../imports/collections/algos';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    Meteor.publish('algos', function() {
+        return Algos.find({ ownerId: this.userId });
+    });
 });
